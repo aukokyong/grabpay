82fdd08ae89cc1dbc3d0c3887fda633c89f016c1
aukokyong <75824065+aukokyong@users.noreply.github.com>
2021-03-18 17:56:51 +0800

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
 import Home from "./components/pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Navibar from "./components/layout/Navibar";
+import Routes from "./components/routes/Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [isLoggedin, setIsloggedin] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("/session/check", { withCredentials: true })
+      .then((response) => {
+        console.log("checking login", response.data);
+        setIsloggedin(response.data);
+      })
+      .catch((error) => {
+        setIsloggedin(false);
+        console.log(error.response);
+      });
+  }, [isLoggedin]);
+
   return (
     <>
-      <Home />
+      <Router>
+        {isLoggedin ? (
+          <>
+            <Navibar setIsloggedin={setIsloggedin} />
+            <Routes />
+          </>
+        ) : (
+          <Home setIsloggedin={setIsloggedin} />
+        )}
+      </Router>
     </>
   );
 }
