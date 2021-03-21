cefa9a46c631c6505341e1c8abeb401749bc377c
aukokyong <75824065+aukokyong@users.noreply.github.com>
2021-03-21 22:14:06 +0800

    deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const mongoose = require("mongoose");
 const db = mongoose.connection;
@@ -44,5 +45,12 @@ app.use("/transfer", TransferController);
 
 const TransactionController = require("./controllers/TransactionController");
 app.use("/transaction", TransactionController);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 4000);
